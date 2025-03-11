8fd0c322abb6cced2faa034b7be8a2703f4224b9
Oussama Moumni <moumnioussama96@gmail.com>
2025-03-11 14:03:47 +0100

    Remove frontend static serving from backend5

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
+const PORT = process.env.PORT || 5000;
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const membersRouter = require("./routes/members");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
   res.send("Backend is live!");
 });
@@ -13,9 +13,9 @@ app.use(
     origin: "https://meraguen-charity-frontend.netlify.app/",
   })
 );
-app.use((req, res) => {
-  res.status(404).send("Route not found");
-});
+// app.use((req, res) => {
+//   res.status(404).send("Route not found");
+// });
 app.use(express.json());
 
 app.use("/api/members", membersRouter);
